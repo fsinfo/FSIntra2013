225822b673c2b193e437c1b22974dd9df3f5d3d4
Manuel Hoffmann <manuel@polythematik.de>
2013-04-06 19:55:15 +0200

    Initialized new rails 4.0.0-beta1 app "fsintra" and added .rvmrc for ruby-2.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fsintra::Application.config.secret_key_base = '039311d4fe3046de4828ef9c04aa3aa1fe31cf2b4b0d547eec33615432dcf4142eb36b5d7805d4fa832b677444e830032d088f9647e8dfb7bf581799449416a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fsintra::Application.config.session_store :encrypted_cookie_store, key: '_fsintra_session'
