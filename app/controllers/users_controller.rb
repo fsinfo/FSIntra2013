fcbeb1ab2d94065ebc3138a5ca9214b650171910
Ayonix <adrianleva@gmail.com>
2013-04-09 18:13:50 +0200

    Getting groups from LDAP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_filter :signed_in_user
-  before_filter :correct_user, only: [:edit,:update]
+  before_action :signed_in_user
+  before_action :correct_user, only: [:edit,:update]
 
   # GET /users
   def index
