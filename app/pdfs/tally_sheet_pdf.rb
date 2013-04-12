978ef22b6ea4b840173cf194b4699377ae698acf
Ayonix <adrianleva@gmail.com>
2013-04-12 23:43:32 +0200

    Tweaked generated pdf

--- app/pdfs/tally_sheet_pdf.rb
+++ app/pdfs/tally_sheet_pdf.rb
@@ -6,17 +6,19 @@ class TallySheetPdf < Prawn::Document
 
     # set up table
     data = Array.new(users.count+1) { Array.new(beverages.count+1) }
+
     # first row
     for i in (1..beverages.count) do
       data[0][i] = beverages[i-1]
     end
 
+    # first column
     for i in (1..users.count) do
       data[i][0] = users[i-1]
     end
 
-    table(data, :header => true) do |table|
-      table.column_widths = table.width/(table.column_length)
+    table(data, :header => true, :width => bounds.width) do |table|
+      table.column_widths = table.width/table.column_length
     end
   end
 end
