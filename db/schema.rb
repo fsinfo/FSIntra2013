9b71d46f70f6ed67cf01dee5456b6b78f82632e9
Manuel Hoffmann <manuel@polythematik.de>
2013-05-09 11:07:52 +0200

    Just for convenience reasons..

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130416071841) do
+ActiveRecord::Schema.define(version: 20130422182630) do
 
   create_table "beverage_tabs", force: true do |t|
     t.integer  "tab_id"
