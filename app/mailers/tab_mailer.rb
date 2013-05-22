696c5bb3bad5b25d01b09c09625945a5950c64cf
Ayonix <adrianleva@gmail.com>
2013-05-22 12:07:51 +0200

    Reworked TabMailer

--- app/mailers/tab_mailer.rb
+++ app/mailers/tab_mailer.rb
@@ -1,4 +1,5 @@
 class TabMailer < ActionMailer::Base
+	KuehlschrankMail = 'kuehlschrank@fachschaft.cs.uni-kl.de'
 
 	# kuehlschrank accepts a tab as paid
 	def paid_email(tab)
@@ -6,35 +7,32 @@ class TabMailer < ActionMailer::Base
 		@tab = tab
 		to = get_email_with_name(@user)
 		mail(
-			from: 'kuehlschrank@fachschaft.cs.uni-kl.de',
+			from: KuehlschrankMail,
 			to: to,
 			subject: 'Eine deiner Rechnungen ist als bezahlt markiert'
 			)
 	end
 
 	# kuehlschrank submitted a tally sheet
-	def tab_email(from_user, to_user, tab)
-		@user = to_user
-		@from_user = from_user
+	def tab_email(tab)
+		@user = tab.user
 		@tab = tab
 		@beverage_tabs = tab.beverage_tabs
 		to = get_email_with_name(@user)
-		from = get_email_with_name(@from_user)
 		mail(
-			from: from,
+			from: KuehlschrankMail,
 			to: to,
 			subject: 'Es gibt eine neue Getränkeabrechnung'
 			)
 	end
 
 	# a user marks a tab as paid
-	def marked_as_paid_email(from_user, tab)
-		@from_user = from_user
-		to = 'kuehlschrank@fachschaft.cs.uni-kl.de'		
+	def marked_as_paid_email(tab)
+		@from_user = tab.user
 		@tab = tab
 		mail(
 			from: get_email_with_name(from_user),
-			to: to,
+			to: KuehlschrankMail,
 			subject: "#{@from_user.displayed_name} hat eine Rechnung als bezahlt markiert"
 			)
 	end
