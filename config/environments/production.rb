bb5bcc82bb07688317b23523dc194022e4a9b681
Ayonix <adrianleva@gmail.com>
2013-06-29 16:18:47 +0200

    Mail settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Fsintra::Application.configure do
   
   # Force ssl
   config.force_ssl = true
+
+  # Mail settings
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@lallafa.fachschaft.cs.uni-kl.de'}
 end
