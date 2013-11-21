7cae14be54de5e26015301d1550392aecaa2cd08
Manuel Hoffmann <manuel@polythematik.de>
2013-11-21 19:03:02 +0100

    idea of motions

--- app/models/minutes/motion.rb
+++ app/models/minutes/motion.rb
@@ -28,4 +28,17 @@ class Minutes::Motion < ActiveRecord::Base
   validates_presence_of :mover
   validates_presence_of :rationale
   validates_presence_of :pro, :con, :abs
+
+  # Prints the Cent-value as €-String
+  # e.g.
+  #   m = Motion.new
+  #   m.amount = 9998
+  #   m.currency
+  #   # => '99,98 €'
+  def amount_in_currency
+  	a = amount / 100
+  	b = amount % 100
+  	b = "0#{b}" if b < 10
+  	"#{a},#{b} €"
+  end
 end
