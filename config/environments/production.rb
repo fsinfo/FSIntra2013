ff0c43befef0ef7d5fa9afdd9c3c8dabba40574b
Manuel Hoffmann <manuel@polythematik.de>
2013-11-27 20:07:24 +0100

    Investigating...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Fsintra::Application.configure do
   # Defaults to:
   config.action_mailer.sendmail_settings = {
      location: '/usr/sbin/sendmail',
-     arguments: '-i -t'
+     arguments: '-i'
   }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
