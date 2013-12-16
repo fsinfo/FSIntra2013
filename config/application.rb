3817574502a39e3b1647edcf66a23ee51a01cc88
Ayonix <adrian.leva@gmail.com>
2013-12-16 16:13:14 +0100

    Set the host for email-urls in production.rb

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,5 @@ module Fsintra
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
-
-    # TODO: set the real host
-    config.action_mailer.default_url_options = { host: 'example.com' }
   end
 end
