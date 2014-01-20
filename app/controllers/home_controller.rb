da1818aaea4193bb9a8bd0df076b4a1fe32b26c3
Ayonix <adrian.leva@gmail.com>
2014-01-20 17:31:50 +0100

    Modified charts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
     @running_tab = current_user.tabs.running.includes(:beverage_tabs).first
     @unpaid_tabs = current_user.tabs.unpaid.includes(:beverage_tabs) 
     @birthday_people = Person.all.sort_by(&:days_to_next_birthday).first(5)
-    @hourly_stat_data = BeverageStats.get_hourly_stats(48)
-    @daily_stat_data = BeverageStats.get_daily_stats(3)
+    @hourly_stat_data = BeverageStats.get_hourly_stats(72)
+    @daily_stat_data = BeverageStats.get_daily_stats(14)
   end
 end
