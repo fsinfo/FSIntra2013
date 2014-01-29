428a4de63cd21265d8d2e99e78ea6a75e142ba32
Ayonix <adrian.leva@gmail.com>
2014-01-29 22:22:23 +0100

    Fixed it

--- app/controllers/minutes/plenum_minutes_controller.rb
+++ app/controllers/minutes/plenum_minutes_controller.rb
@@ -1,41 +1,46 @@
 class Minutes::PlenumMinutesController < Minutes::MinutesController
   before_action :trim_guests, only: [:update, :create]
-  load_and_authorize_resource
   respond_to :html, :json
   
   def new
-    @minutes_minute = Minutes::PlenumMinute.new
-    respond_with(@minutes_minute)
+    respond_with(@plenum_minute)
   end
 
   # POST /minutes/minutes
   # POST /minutes/minutes.json
   def create
     params[:minutes_plenum_minute][:keeper_of_the_minutes_id] ||= current_user.id
-    @minutes_minute = Minutes::PlenumMinute.new(minutes_minute_params)
+    @plenum_minute = Minutes::PlenumMinute.new(minutes_plenum_minute_params)
 
     puts "\n\n\n"
-    puts @minutes_minute
+    puts @plenum_minute.inspect
     puts "\n\n\n"
 
-    if @minutes_minute.save
-      @forward = url_for [:edit, @minutes_minute]      
-      respond_with(@minutes_minute, status: :created, :location => @minutes_minute)
+    if @plenum_minute.save
+      @forward = url_for [:edit, @plenum_minute]      
+      puts '##########'
+      puts 'SAVED'
+      puts '##########'
+      respond_with(@plenum_minute, status: :created, :location => @plenum_minute)
     else
-      respond_with(@minutes_minute, status: :unprocessable_entity)
+      puts '##########'
+      puts 'NOT SAVED'
+      puts '##########'
+      respond_with(@plenum_minute, status: :unprocessable_entity)
     end
   end
 
   private
 
-    def minutes_minute_params
-      params.require(:minutes_plenum_minute).permit!(:date,
+    def minutes_plenum_minute_params
+      params.require(:minutes_plenum_minute).permit(:date,
                                              :chairperson_id,
+                                             :keeper_of_the_minutes_id,
                                              :guests)
     end
 
     # If someone forgets to remove the last ", "
     def trim_guests
-      params[:minutes_minute][:guests] = params[:minutes_minute][:guests].chomp(", ")
+      params[:minutes_plenum_minute][:guests] = params[:minutes_plenum_minute][:guests].chomp(", ")
     end
 end
