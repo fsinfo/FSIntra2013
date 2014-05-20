58625234bf50dad33c4429acc29fa66a7c4e5082
Ayonix <adrian.leva@gmail.com>
2014-05-20 10:54:16 +0200

    Use LDAP with SSL

--- lib/fs_ldap.rb
+++ lib/fs_ldap.rb
@@ -3,11 +3,12 @@ require 'net/ldap'
 class FsLdap
   HOST = 'ford.fachschaft.informatik.uni-kl.de'
   BASE = 'dc=fachschaft,dc=informatik,dc=uni-kl,dc=de'
-  LDAP = Net::LDAP.new(:host => HOST)
+  PORT = 636
+  LDAP = Net::LDAP.new(:host => HOST, :port => PORT, :encryption => :simple_tls)
 
   def self.get_lastname(loginname)
     filter = Net::LDAP::Filter.eq('cn',loginname)
-    return LDAP.search(:base => BASE, :filter => filter, :attributes => ['sn']).first.sn.first 
+    return LDAP.search(:base => BASE, :filter => filter, :attributes => ['sn']).first.sn.first
   end
 
   def self.get_firstname(loginname)
@@ -26,7 +27,7 @@ class FsLdap
   end
 
   def self.authenticate(loginname, password)
-    lderp = Net::LDAP.new(:host => HOST)
+    lderp = Net::LDAP.new(:host => HOST, :port => PORT, :encryption => :simple_tls)
     lderp.auth("cn=#{loginname},ou=users,#{BASE}",password)
     return lderp.bind
   end
