7cfd5859ad203cbc2ff991502d77cb2a721bbb7c
Frederik Walk <f_walk09@cs.uni-kl.de>
2014-05-25 18:05:15 +0200

    Added gravatars to the tally sheet

--- app/helpers/tally_sheets_helper.rb
+++ app/helpers/tally_sheets_helper.rb
@@ -1,2 +1,6 @@
 module TallySheetsHelper
+	def gravatar_url(user, height)
+		gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+		"https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{height}&d=retro"
+	end
 end
