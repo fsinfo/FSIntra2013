cd0a14c129458cf19056cb71d66d46d4d1cca298
ayonix <ayonix@users.noreply.github.com>
2014-05-28 12:21:39 +0200

    Sort by beverage name instead of the logo

--- app/assets/javascripts/beverages.js
+++ app/assets/javascripts/beverages.js
@@ -1,6 +1,7 @@
 $(function() {
 	$('#beverages').dataTable({
 		"aoColumnDefs" : [ {'bSortable' : false,'aTargets' : [ 6 ]} ],
+		"aaSorting": [[1,"asc"]],
 		"bPaginate": false,
 		"bInfo": false
 	});
