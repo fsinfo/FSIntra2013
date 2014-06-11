9c75f2e468543f7f7a2c2ba57bfb8953135df77c
Ayonix <adrian.leva@gmail.com>
2014-06-11 10:49:01 +0200

    beverages/index done

--- app/assets/javascripts/beverages.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('#beverages').dataTable({
-		"aoColumnDefs" : [ {'bSortable' : false,'aTargets' : [ 7 ]} ],
-		"aaSorting": [[1,"asc"]],
-		"bPaginate": false,
-		"bInfo": false
-	});
-});
